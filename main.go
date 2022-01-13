23368ef23c3db63f0886c3ba660f31f20c7f3131
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2022-01-13 20:10:13 +0000

    update

--- main.go
+++ main.go
@@ -2,27 +2,26 @@ package main
 
 import (
 	//"fmt"
-	
+
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 type event struct {
-	id int
-	name string
-	eventtype string 
-	description string 
-	img string 
+	id          int
+	name        string
+	eventtype   string
+	description string
+	img         string
 }
 
 func main() {
-	
-	
 
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.LoadHTMLGlob("static/templates/*.html")
-	
+
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{"navtitle": "VentureOut."})
 	})
@@ -31,5 +30,5 @@ func main() {
 		c.HTML(http.StatusOK, "index.html", gin.H{"navtitle": "VentureOut."})
 	})
 
-	r.Run(":8080")
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
