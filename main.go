6dab20fabcd0e849ff52726ab6326dbcf497c2c3
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2022-01-13 17:19:11 +0000

    Testing page Routing

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	//"fmt"
+	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
+type event struct {
+	id int
+	name string
+	eventtype string 
+	description string 
+	img string 
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.LoadHTMLGlob("static/templates/*.html")
+	
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{"navtitle": "VentureOut."})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.Run(":8080")
 }
