b92a91d5fe6fa024887629f4cb82023aba0eede4
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2022-02-05 17:50:35 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -16,8 +16,32 @@ self.addEventListener('activate', function(event) {
 
 
 const CacheUrls = [
-    "/", 
-    "app.js", 
-    "styles.css", 
-    "logo.svg"
+    './',
+    '/static/css/main.css', 
+    '/static/css/mdb.dark.min.css',
+    '/static/css/mdb.dark.rtl.min.css',
+    '/static/css/mdb.min.css',
+    '/static/css/mdb.rtl.min.css',
+    '/static/img/favicon.ico',
+    '/static/img/logo.png',
+    '/static/img/maskable_icon_x72.png',
+    '/static/img/maskable_icon_x96.png',
+    '/static/img/maskable_icon_x128.png',
+    '/static/img/maskable_icon_x192.png',
+    '/static/js/html5-qrcode.min.js',
+    '/static/js/mdb.min.js',
+    'manifest.webmanifest',
+    'service-worker.js',
+    '/static/HWUevents.geojson',
+    '/static/templates/addevent.html',
+    '/static/templates/footer.html',
+    '/static/templates/header.html',
+    '/static/templates/index.html',
+    '/static/templates/infomodal.html',
+    '/static/templates/map.html',
+    '/static/templates/navbar.html',
+    '/static/templates/navbarmobile.html',
+    '/static/templates/scan.html',
+    '/static/templates/scripts.html',
+    '/static/templates/settings.html'
 ];
