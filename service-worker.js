01659e50b5f26e5b17e68ed9862603d017e4a0cc
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2022-02-05 17:37:25 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
 console.log('service worker getting out of bed and having a coffee');
+
+
+self.addEventListener("install", function(event) {
+   event.waitUntil(
+      caches.open("VentureOut-cache")
+      .then(cache => {
+         return cache.addAll(CacheUrls);
+      })
+   );
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('Service worker has been activated')
+});
+
+
+const CacheUrls = [
+    "/", 
+    "app.js", 
+    "styles.css", 
+    "logo.svg"
+];
