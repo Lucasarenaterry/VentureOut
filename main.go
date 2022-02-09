33ccd37b02e52c60a6e2e74883612a647835ad42
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2022-02-09 23:39:54 +0000

    Events are loaded onto map from DB!!!! :)

--- main.go
+++ main.go
@@ -16,8 +16,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-
-
 type Event struct {
 	Id          int `json:"Id"`
 	Eventtittel string `json:"Eventtittel"`
@@ -118,38 +116,36 @@ func main() {
 	//VALUES (1, 'LANDSCAPE TRAIL', 'Walk', 'walk around campus visiting the main landcapes', 'landscape.png', 'SRID=4326;POINT(-3.321578 55.910807)', '2022/01/23');
 
 	r.GET("/map", func(c *gin.Context) {
-			// if _, err := db.Exec("CREATE TABLE IF NOT EXISTS Events ticks (tick timestamp)"); 
-			// 		err != nil {
-			// 	c.String(http.StatusInternalServerError,
-			// 		fmt.Sprintf("Error creating database table: %q", err))
-			// 	return
-			// }
-   
-			// if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); 
-			// 	err != nil {
-			// 	c.String(http.StatusInternalServerError,
-			// 		fmt.Sprintf("Error incrementing tick: %q", err))
-			// 	return
-			// }
-   
-			// rows, err := db.Query("SELECT tick FROM ticks")
-			// if err != nil {
-			// 	c.String(http.StatusInternalServerError,
-			// 		fmt.Sprintf("Error reading ticks: %q", err))
-			// 	return
-			// }
-   
-			// defer rows.Close()
-			// for rows.Next() {
-			// 	var tick time.Time
-			// 	if err := rows.Scan(&tick); err != nil {
-			// 		c.String(http.StatusInternalServerError,
-			// 			fmt.Sprintf("Error scanning ticks: %q", err))
-			// 		return
-			// 	}
-			// 	c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
-			// }
-			c.HTML(http.StatusOK, "map.html", gin.H{})
+			if _, err := db.Exec("CREATE TABLE IF NOT EXISTS Events (id SERIAL PRIMARY KEY, eventtittel varchar(45) NOT NULL, eventtype varchar(45) NOT NULL, description varchar(255) NOT NULL, image TEXT, location GEOMETRY(POINT,4326), eventdate DATE, eventtime TIME)"); 
+				err != nil {
+					c.String(http.StatusInternalServerError,
+					fmt.Sprintf("Error creating database table: %q", err))
+				return
+			}
+
+			rows, err := db.Query("SELECT json_build_object( 'type', 'FeatureCollection', 'features', json_agg( json_build_object( 'type', 'Feature', 'properties', to_jsonb( t.* ) - 'location', 'geometry', ST_AsGeoJSON(location)::jsonb ) ) ) as json from events as t(id, eventtittel, eventtype, description, image, location, eventdate, eventtime)")
+				if err != nil {
+					c.String(http.StatusInternalServerError,
+					fmt.Sprintf("Error reading Events: %q", err))
+				return
+			}
+			
+			var featureCollection string
+
+			defer rows.Close()
+			
+			for rows.Next() {
+				if err := rows.Scan(&featureCollection); 
+					err != nil {
+						c.String(http.StatusInternalServerError,
+							fmt.Sprintf("Error scanning events: %q", err))
+						return
+					}
+			}
+			fmt.Printf("%v", featureCollection)
+			
+			
+			c.HTML(http.StatusOK, "map.html", gin.H{ "featureCollection": featureCollection, })
 		})
 
 		r.GET("/addevent", func(c *gin.Context) {
@@ -161,6 +157,13 @@ func main() {
 		})
 
 		r.GET("/scan", func(c *gin.Context) {
+
+
+
+
+
+
+
 			c.HTML(http.StatusOK, "scan.html", gin.H{})
 		})
 	
